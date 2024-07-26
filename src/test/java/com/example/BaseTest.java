de1436425bf34b57567c2d0c193bfe66e359052d
Daria Potorochina <dariyasnow1806@mail.ru>
2024-07-26 12:15:01 +0300

    написаны основные тесты для классов

--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -1,2 +1,12 @@
-package com.example;public class BaseTest {
+package com.example;
+
+import org.junit.Before;
+import org.mockito.MockitoAnnotations;
+
+public class BaseTest {
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
 }
